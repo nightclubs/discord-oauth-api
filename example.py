f96f7111d09fb716763d3f288244a3f45d847bcf
zt <116030602+nightclubs@users.noreply.github.com>
2022-11-17 19:38:17 -0500

    Update example.py

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/callback')
 def callback():
   code = request.args.get('code'); token = lol.oauth(client_id, 'client_secret', 'redirect_url', code)
-  return redirect('https://github.com/accusable/discord-oauth-api')
+  return redirect('https://github.com/nightclubs/discord-oauth-api')
 
 
     
